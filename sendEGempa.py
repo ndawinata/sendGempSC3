b5bc9f0de3778fb11dbe3ac293bd4c54bb0be5e0
ndawinata <nanda.winata@gmail.com>
2022-07-27 06:26:49 +0800

    uuuppp

--- sendEGempa.py
+++ sendEGempa.py
@@ -9,6 +9,7 @@ import os
 url = 'http://36.91.187.178:8000/infogempa'
 R = 6373.0
 
+
 def measure(llat1, llon1, llat2, llon2):
 
     lat1 = radians(llat1)
@@ -27,7 +28,7 @@ def measure(llat1, llon1, llat2, llon2):
     return distance
 
 def keterangan(plat, plon):
-    df = pd.read_csv('location.txt', delimiter=' ')
+    df = pd.read_csv('/home/sysop/alarm/location.txt', delimiter=' ')
     df.columns = ["lat", "lon", "kota"]
     # print(df.tail)
     arr = []
@@ -44,6 +45,7 @@ def keterangan(plat, plon):
 
     return "(%s Km %s)" % (dist, kota)
 
+
 parser = argparse.ArgumentParser(description='A test program.')
 
 parser.add_argument("-p", "--print_string", help="Prints the supplied argument.", action="store_true")
@@ -52,20 +54,20 @@ parser.add_argument('-m', action="store", dest="mag")
 parser.add_argument('-t', action="store", dest="tgl")
 parser.add_argument('-lok', action="store", dest="lok")
 parser.add_argument('-lat', action="store", dest="lat")
-parser.add_argument('-lon', action="store", dest="lon")
+parser.add_argument('-lon', action="store", dest="long")
 parser.add_argument('-d', action="store", dest="depth")
 
 args = parser.parse_args()
 
 text = 'Info Gempa Mag:{} SR, {}, Lok: {} {}, Kedlmn: {} Km ::BMKG-PGR VIII'
-textinfo = (text.format(args.mag, args.tgl, args.lok, keterangan(args.lat, args.lon),args.depth))
+textinfo = (text.format(args.mag, args.tgl, args.lok, keterangan(float(args.lat), float(args.long)), args.depth))
 
 myobj = {
     'text':textinfo,
     'tsunami':False
 }
 
-file1 = open("logGempa.txt", "a")  # append mode
+file1 = open("/home/sysop/logGempa.txt", "a")  # append mode
 
 try:
     res = requests.post(url, json=myobj)
@@ -82,4 +84,3 @@ except:
     os.system('notify-send "Seiscomp3" "Gagal Mengirim parameter ke E-Gempa..."')
     file1.write("gagal %s  \n" % textinfo)
     file1.close()
-
